d42bd13e7c5d8aff92ce26624a6650ddcd3109fd
fiko942 <tobellord@gmail.com>
2024-01-30 23:47:56 +0700

    init session engine

--- src/Component/AuthWrapper.jsx
+++ src/Component/AuthWrapper.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import DataUsageIcon from "@mui/icons-material/DataUsage";
 import google from "../Icons/google.webp";
+import icon from "../Icons/icon.png";
+import config from "../config.js";
 
 export default function AuthWrapper({ onAuthenticated }) {
   const [loading, setLoading] = useState(false);
@@ -21,13 +23,20 @@ export default function AuthWrapper({ onAuthenticated }) {
       )}
       {!loading && (
         <div className="flex flex-col w-300">
+          <div className="flex items-center gap-2">
+            <img src={icon} alt="Gossip" title="Gossip" className="w-20" />
+            <div className="flex flex-col">
+              <h1 className="text-sky-900 text-xl font-semibold">Gossip</h1>
+              <span className="text-gray-600 italic">v.1</span>
+            </div>
+          </div>
           <div className="p-0.5 rounded bg-gray-200 m-auto flex w-5/6 mb-2"></div>
-          <span className="font-semibold text-xl mb-5">
+          <span className="font-semibold text-lg mb-5">
             Let's connect with other people
           </span>
           <div className="flex flex-col gap-4">
             <a
-              href="/auth/google"
+              href={`${config.api.base}/auth/google?redirect=${window.location.href}`}
               className="flex items-center gap-4 bg-blue-100 rounded-lg py-2 px-4 hover:bg-blue-200 hover:outline-2 hover:outline-sky-500 hover:outline"
             >
               <img
---
+++ src/Utils/Session.js
@@ -0,0 +1,4 @@
+import config from "../config";
+import axios from "axios";
+
+export default class Session {}
